f2218ceac6f481cbe751f030d8e3d871718baec2
Hadi Ahmadi <MacPersia@gmail.com>
2019-05-22 02:37:13 +0800

    openning 8080

--- az-prep.sh
+++ az-prep.sh
@@ -1,2 +1,4 @@
 az vm create --resource-group planty-resource-group --name my-docker-vm --image UbuntuLTS --admin-username azureuser --generate-ssh-key --custom-data az-vm-custom-data.txt
 
+az vm open-port --port 8080 --resource-group planty-resource-group --name my-docker-vm
+
