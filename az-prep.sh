9721a4505f136a2d1fccd37a55157a57cc0ad2cc
Hadi Ahmadi <MacPersia@gmail.com>
2019-06-07 01:28:52 +0800

    Increased vm size, to accomodate MS Sql Server

--- az-prep.sh
+++ az-prep.sh
@@ -1,5 +1,5 @@
-#az vm create --resource-group planty-resource-group --name vm-income-estimate --size Standard_A1_v2 --image Canonical:UbuntuServer:18.04-LTS:latest --admin-username azureuser --generate-ssh-key --custom-data az-vm-custom-data.txt --storage-sku Standard_LRS --os-disk-size-gb 10
-az vm create --resource-group planty-resource-group --name vm-income-estimate --size Standard_A1_v2 --image Canonical:UbuntuServer:18.04-LTS:latest --admin-username azureuser --generate-ssh-key --custom-data az-vm-custom-data.txt --storage-sku Standard_LRS
+#az vm create --resource-group planty-resource-group --name vm-income-estimate --size Standard_B2s --image Canonical:UbuntuServer:18.04-LTS:latest --admin-username azureuser --generate-ssh-key --custom-data az-vm-custom-data.txt --storage-sku Standard_LRS --os-disk-size-gb 10
+az vm create --resource-group planty-resource-group --name vm-income-estimate --size Standard_B2s --image Canonical:UbuntuServer:18.04-LTS:latest --admin-username azureuser --generate-ssh-key --custom-data az-vm-custom-data.txt --storage-sku Standard_LRS
 
 az vm open-port --port 8080 --resource-group planty-resource-group --name vm-income-estimate
 
